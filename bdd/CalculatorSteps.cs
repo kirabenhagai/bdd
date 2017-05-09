4b65aa2a6bd6a2376165d406124bcd4048012695
kira <kira@sears.co.il>
2017-05-09 22:48:28 +0300

    BDD - first commit

---
+++ bdd/CalculatorSteps.cs
@@ -0,0 +1,37 @@
+﻿using codeForBdd;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace bdd
+{
+	[Binding]
+	public class CalculatorSteps
+	{
+		private int Result { get; set; }
+		private readonly Calculator _calculator = new Calculator();
+
+		[Given(@"I have entered (.*) into the calculator")]
+		public void GivenIHaveEnteredIntoTheCalculator(int number)
+		{
+			_calculator.FirstNumber = number;
+		}
+
+		[Given(@"I have also entered (.*) into the calculator")]
+		public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+		{
+			_calculator.SecondNumber = number;
+		}
+
+		[When(@"I press add")]
+		public void WhenIPressAdd()
+		{
+			Result = _calculator.Add();
+		}
+
+		[Then(@"the result should be (.*) on the screen")]
+		public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+		{
+			Assert.AreEqual(expectedResult, Result);
+		}
+	}
+}
